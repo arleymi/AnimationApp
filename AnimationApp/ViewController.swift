75e3931ffe8c6c5d901fe0ce2fd9e2519a56374e
Arthur <arthur.lee290499@gmail.com>
2021-07-26 23:24:58 +0600

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Arthur Lee on 26.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
